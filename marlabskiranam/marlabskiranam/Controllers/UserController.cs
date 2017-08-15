8aec04b2639db487826c4cdddc808261ee119e3f
aakuthota venkatesh <avenkat.qa@gmail.com>
2017-08-15 01:02:38 -0400

    Home,Login,Singup and profile page is done.. Need to decorate them, but functionality works fine

---
+++ marlabskiranam/marlabskiranam/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using marlabskiranam.Models;
+
+namespace marlabskiranam.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signin(login user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (kiranamContext kc = new kiranamContext())
+                {
+
+                    var v = (kc.mvc_users.Where(c => c.EmailID == user.EmailID && c.Password == user.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["username"] = user.EmailID;
+                        return RedirectToAction("Profilepage","User");
+                    }
+                    else
+                    {
+                        Response.Write("Invalid Credentials");
+                        return RedirectToAction("Signin", "User");
+                    }
+                }
+
+            }
+            
+            return View();
+        }
+
+        public ActionResult Profilepage()
+        {
+            if (Session["username"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Signin","User");
+            } 
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
---
+++ marlabskiranam/marlabskiranam/Models/login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace marlabskiranam.Models
+{
+    public class login
+    {
+        public string EmailID { get; set; }
+        public string Password { get; set; }
+    }
+}
