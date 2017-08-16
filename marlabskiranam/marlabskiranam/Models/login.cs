eab6e72cf3a1e82bf58eb17ce1c5f1ea1ad1cb70
aakuthota venkatesh <avenkat.qa@gmail.com>
2017-08-16 09:59:02 -0400

    updated login page validation

--- marlabskiranam/marlabskiranam/Models/login.cs
+++ marlabskiranam/marlabskiranam/Models/login.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace marlabskiranam.Models
 {
     public class login
     {
+        [DisplayName("Email ID")]
+        [Required(ErrorMessage ="EmailID required")]
+        [DataType(DataType.EmailAddress)]
         public string EmailID { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        
     }
 }
